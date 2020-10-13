d1c2b887359cdab9b7b5adf215cc6f5f191bfd35
Anubhav Choudhary <70624944+achoudhary015@users.noreply.github.com>
2020-10-13 00:29:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 '''------------------------------------
 #Programming Language used :: Python
 #Author :: Anubhav Choudhary
-#Email :: anubhavchoudhary009@gmail.com
-#Website ::https://anubhavchoudhary009.github.io/portfolio.me/
 ---------------------------------------'''
 ########################################
 
